34d3b13f0a82593f44de15b3e757ac69f417d7b6
SleepyMorfeusz <Sleepymorfeusz@gmail.com>
2023-11-03 18:23:13 +0100

    Create and style present app

--- script/main-page.js
+++ script/main-page.js
@@ -1,6 +1,7 @@
 const appContainers = document.querySelectorAll(".app-container");
 const appPopups = document.querySelectorAll(".app-popup");
 const closeButtons = document.querySelectorAll(".closeButton");
+const code = document.getElementById("code");
 
 appPopups.forEach((popup) => {
   popup.style.display = "none";
@@ -20,4 +21,11 @@ closeButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
     appPopups[index].style.display = "none";
   });
+});
+
+//code script
+
+code.addEventListener("click", function () {
+  code.textContent = "NEWIR-GC5QW-2WG0G";
+  code.style.fontSize = "3rem";
 });
